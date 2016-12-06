7a801b389fbe6b5b2d6c83581fe53d23ab3e928e
rajith123 <rajith.lakashan11@gmail.com>
2016-12-06 12:42:38 +0530

    Add files via upload

---
+++ RISCV_IoT/src/test/scala/examples/test/pc_mux.scala
@@ -0,0 +1,36 @@
+import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
+
+class pc_muxTests(c: pc_mux) extends PeekPokeTester(c) {
+  for (s0 <- 0 until 2) {
+    for (s1 <- 0 until 2) {
+      for(i0 <- 0 until 2) {
+        for(i1 <- 0 until 2) {
+          for(i2 <- 0 until 2) {
+            for(i3 <- 0 until 2) {
+              poke(c.io.pc_sel, s1 << 1 | s0)
+              poke(c.io.pc_4, i0)
+              poke(c.io.pc, i1)
+              poke(c.io.jmp_br, i2)
+              poke(c.io.pc_4, i3)
+              step(1)
+              val out = if(s1 == 1) {
+                          if (s0 == 1) i3 else i2
+                        } else {
+                          if (s0 == 1) i1 else i0 
+                        }
+              expect(c.io.to_pc, out)
+            }
+          }
+        }
+      } 
+    }
+  }
+}
+
+class pc_muxTester extends ChiselFlatSpec {
+  behavior of "pc_mux"
+  backends foreach {backend =>    it should s"correctly add randomly generated numbers $backend" in {
+      Driver(() => new pc_mux())(c => new pc_muxTests(c)) should be (true)
+    }
+  }
+}

ed242c8be27004d15ac6878be8e9ea34af159775
rajith123 <rajith.lakashan11@gmail.com>
2016-12-06 12:32:53 +0530

    Add files via upload

---
+++ main/scala/jmp_jalr_br_mux.scala
@@ -0,0 +1,11 @@
+import chisel3._
+
+class jmp_br_jalr_mux extends Module {
+  val io = IO(new Bundle {
+    val br_jmp 				= Input(UInt(width = 32))
+    val jalr 				= Input(UInt(width = 32))
+    val jmp_br_jalr_mux_sel = Input(UInt(width = 1))
+    val to_pc_mux 			= Output(UInt(width = 32))
+  })
+  io.to_pc_mux := Mux( jmp_br_jalr_mux_sel , jalr , br_jmp )
+}

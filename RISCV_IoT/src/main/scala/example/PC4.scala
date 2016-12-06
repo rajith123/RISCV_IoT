34d4a5485aca2860ee8a2e2ee9a92213e4796d9c
Maduwantha92 <a.a.s.d.maduwantha@gmail.com>
2016-12-06 13:07:08 +0530

    Add files via upload

---
+++ RISCV_IoT/src/main/scala/example/PC4.scala
@@ -0,0 +1,10 @@
+import chisel3._
+
+class PC4 extends Module {
+  val io = IO(new Bundle {
+    val PC4_in    = Input(UInt(width=32))
+    val PC4_out   = Output(UInt(width=32))
+  })
+
+  io.PC4_out := Reg(next = io.PC4_in)
+}
---
+++ RISCV_IoT/src/main/scala/example/RegFile.scala
@@ -0,0 +1,24 @@
+import chisel3._
+
+class RegFile extends Module {
+  val io = IO(new Bundle {
+	val regWidth = parms.get[Int]("widthRegFile")
+	val regDepth = parms.get[Int]("depthRegFile")
+    val rs_in_adr    = Input(UInt(width=4))
+    val rs1_out_addr   = Input(UInt(width=4))
+	val rs2_out_addr   = Input(UInt(width=4))
+	val rs1_out_data	= Output(Uint(width=32))
+	val rs2_out_data 	= Output(Uint(width=32))
+	val rs_in_data 	= Input(Uint(width=32))
+	val wen		 = Input(Uint(width=1))
+  })
+  val regFile = Vec.fill(regDepth) { Reg(init = Uint(0, width = regWidth))}
+  
+  rs1_out_data := regFile(rs1_out_addr)
+  rs2_out_data := regFile(rs2_out_addr)
+  
+  when (wen){
+	regFile(rs_in_adr) := rs_in_data;
+	}
+
+}

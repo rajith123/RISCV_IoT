e21dc7c82f4e4890a853e7e36cda80917b0c4e17
Maduwantha92 <a.a.s.d.maduwantha@gmail.com>
2016-12-06 13:15:23 +0530

    Update PC4.scala

--- RISCV_IoT/src/test/scala/examples/test/PC4.scala
+++ RISCV_IoT/src/test/scala/examples/test/PC4.scala
@@ -1,11 +1,11 @@
 import chisel3.iotesters.{PeekPokeTester, Driver, ChiselFlatSpec}
 
 class PC4Tests(c: PC4) extends Tester(c) {
-  val test_in = 119
   for (t <- 0 until 4) {
+     val test_in = rnd.nextInt(32)
     poke(c.io.PC4_in,     test_in)
     step(1)
-    expect(c.io.PC4_out, (test_in << t) & 0xFFFF)
+    expect(c.io.PC4_out, test_in)
   }
 }
 

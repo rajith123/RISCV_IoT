0cb9c9d52f8cb2af4873748fe436edd5812c2766
Maduwantha92 <a.a.s.d.maduwantha@gmail.com>
2016-12-07 13:47:12 +0530

    Update RegFile.scala

--- RISCV_IoT/src/main/scala/example/RegFile.scala
+++ RISCV_IoT/src/main/scala/example/RegFile.scala
@@ -4,13 +4,13 @@ class RegFile extends Module {
   val io = IO(new Bundle {
 	val regWidth = 32
 	val regDepth = 16
-    	val rs_in_adr    = Input(UInt(width=4))
-    	val rs1_out_addr   = Input(UInt(width=4))
-	val rs2_out_addr   = Input(UInt(width=4))
-	val rs1_out_data	= Output(Uint(width=32))
-	val rs2_out_data 	= Output(Uint(width=32))
-	val rs_in_data 	= Input(Uint(width=32))
-	val wen		 = Input(Uint(width=1))
+    	val rs_in_adr = Input(UInt(width=4))
+    	val rs1_out_addr = Input(UInt(width=4))
+	val rs2_out_addr = Input(UInt(width=4))
+	val rs1_out_data = Output(Uint(width=32))
+	val rs2_out_data = Output(Uint(width=32))
+	val rs_in_data = Input(Uint(width=32))
+	val wen = Input(Uint(width=1))
   })
   val regFile = Vec.fill(regDepth) { Reg(init = Uint(0, width = regWidth))}
   

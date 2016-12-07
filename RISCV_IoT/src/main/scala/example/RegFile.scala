ce72cf47da02507ab995412a8e42b3be9329e17b
Maduwantha92 <a.a.s.d.maduwantha@gmail.com>
2016-12-07 13:45:05 +0530

    Update RegFile.scala

--- RISCV_IoT/src/main/scala/example/RegFile.scala
+++ RISCV_IoT/src/main/scala/example/RegFile.scala
@@ -2,10 +2,10 @@ import chisel3._
 
 class RegFile extends Module {
   val io = IO(new Bundle {
-	val regWidth = parms.get[Int]("widthRegFile")
-	val regDepth = parms.get[Int]("depthRegFile")
-    val rs_in_adr    = Input(UInt(width=4))
-    val rs1_out_addr   = Input(UInt(width=4))
+	val regWidth = 32
+	val regDepth = 16
+    	val rs_in_adr    = Input(UInt(width=4))
+    	val rs1_out_addr   = Input(UInt(width=4))
 	val rs2_out_addr   = Input(UInt(width=4))
 	val rs1_out_data	= Output(Uint(width=32))
 	val rs2_out_data 	= Output(Uint(width=32))

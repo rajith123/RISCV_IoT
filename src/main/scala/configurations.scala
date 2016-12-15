954631c23accc0b51fd446a99e8840393c7c569a
Isuru Rathnayaka <isururathnayaka@gmail.com>
2016-12-15 12:59:38 +0530

    adding configurations file and changing alu to package core

---
+++ src/main/scala/configurations.scala
@@ -0,0 +1,19 @@
+package Common
+{
+import chisel3._
+//import Node._
+
+//import Constants._
+   
+case class SodorConfiguration()
+{
+   val xprlen = 32
+   val nxpr = 32
+   //val nxprbits = log2Up(nxpr)
+   val nxprbits = 5
+   val rvc = false
+   val vm = false
+}
+
+
+}
--- src/main/scala/core/alu.scala
+++ src/main/scala/core/alu.scala
@@ -1,4 +1,4 @@
-package RiscvIoT
+package core
 
 import chisel3._
 import chisel3.util._

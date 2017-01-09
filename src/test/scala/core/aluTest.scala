4bc652ba7de6c10d9bed7f3269b8196026b2bc5b
Isuru Rathnayaka <isururathnayaka@gmail.com>
2017-01-09 19:07:10 +0530

    added alu verilog test bench and tool chain generated files of alu.scala

---
+++ src/test/scala/core/aluTest.scala
@@ -0,0 +1,56 @@
+package RiscvIoT
+
+import Common._
+//import chisel3._
+import chisel3.util._
+import chisel3.iotesters._
+import org.scalatest.{Matchers, FlatSpec}
+//import RiscvIoT.ALU
+
+class ALUUnitTester(c: ALU) extends PeekPokeTester(c) {
+
+  for(i <- 1 to 10) {
+    
+    val in1 = rnd.nextInt(10)
+    val in2 = rnd.nextInt(10)
+    val fn  = 4//rnd.nextInt(2)
+
+    var output = 0
+
+    poke(c.io.in1, in1)
+    poke(c.io.in2, in2)
+    poke(c.io.fn, fn)
+
+    printf("%d %d %d\n", in1, in2, fn)
+
+    step(1)   
+
+    if (fn == 0) {
+      output = (in1+in2)
+    } else if (fn == 1) {
+      output = (in1 << in2)
+    } else if (fn == 4) {
+      output = (in1 ^ in2)
+    } //else if (fn == 5) {
+//    output = 
+//  } else if (fn == 6) {
+//    output = 
+//  } 
+    
+    expect(c.io.out, output)
+    //expect(c.io.adder_out, i+j)
+    printf("%d \n", output)
+  }
+}
+
+class ALUTester extends ChiselFlatSpec {
+  val backendNames = Array[String]("firrtl", "verilator")
+  for ( backendName <- backendNames ) {
+    implicit val conf = new SodorConfiguration
+    "ALU" should s"carry out proper arithmatic and logical operations (with ${backendName})" in {
+      Driver(() => new ALU, backendName) {
+        c => new ALUUnitTester(c)
+      } should be (true)
+    }
+  }
+}

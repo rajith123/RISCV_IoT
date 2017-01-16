cae65c276edfd6f33866849d625968ef7f3bda2b
A.A.S.D.Maduwantha <a.a.s.d.maduwantha@gmail.com>
2017-01-16 17:13:29 +0530

    Decoder bug fix PC_mux

--- test_run_dir/core.pc_muxTester1658438993/pc_mux.v
+++ test_run_dir/core.pc_muxTester1658438993/pc_mux.v
@@ -17,7 +17,7 @@ module pc_mux(
   input  [31:0] io_pc_4,
   input  [31:0] io_pc,
   input  [31:0] io_jmp_br,
-  input  [3:0] io_pc_sel,
+  input  [1:0] io_pc_sel,
   output [31:0] io_to_pc
 );
   wire  _T_12;
@@ -27,9 +27,9 @@ module pc_mux(
   wire [31:0] _T_16;
   wire [31:0] _T_17;
   assign io_to_pc = _T_17;
-  assign _T_12 = io_pc_sel == 4'h0;
-  assign _T_13 = io_pc_sel == 4'h1;
-  assign _T_14 = io_pc_sel == 4'h2;
+  assign _T_12 = io_pc_sel == 2'h0;
+  assign _T_13 = io_pc_sel == 2'h1;
+  assign _T_14 = io_pc_sel == 2'h2;
   assign _T_15 = _T_14 ? io_jmp_br : io_pc_4;
   assign _T_16 = _T_13 ? io_pc : _T_15;
   assign _T_17 = _T_12 ? io_pc_4 : _T_16;
